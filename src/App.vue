79d2c4bfdcfeb165943eee4c7f35800945866ded
Kay-Rick <Kay_Rick@outlook.com>
2023-04-08 22:52:26 +0800

    develop navbar

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,18 @@
 <template>
-  <div>
-    <div>Bot name: {{ bot_name }}</div>
-    <div>Bot rating: {{ bot_rating }}</div>
-  </div>
+  <NavBar/>
   <router-view/>
 </template>
 
 <script>
-import $ from 'jquery';
-import { ref } from 'vue';
+
+import NavBar from './components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 
 export default {
   name: "App",
-  setup: () => {
-    let bot_name = ref("");
-    let bot_rating = ref("");
-
-    $.ajax({
-      url: "http://127.0.0.1:8081/pk/getbotinfo/",
-      type: "get",
-      success: resp => {
-        bot_name.value = resp.name;
-        bot_rating.value = resp.rating;
-      }
-    });
-
-    return {
-      bot_name,
-      bot_rating
-    }
+  components: {
+    NavBar
   }
 }
 
