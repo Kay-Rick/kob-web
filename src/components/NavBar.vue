6fd56927c359c98012531d7fe398c95556c4f899
Kay-Rick <Kay_Rick@outlook.com>
2023-04-18 17:06:18 +0800

    develop login

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,18 +14,31 @@
             <router-link :class="route_name == 'ranklist_index' ? 'nav-link active' : 'nav-link' " :to = "{name: 'ranklist_index'}">排行榜</router-link>
           </li>
         </ul>
-        <ul class="navbar-nav">
+        <ul class="navbar-nav" v-if="$store.state.user.is_login">
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Rick
+              {{ $store.state.user.username }}
             </a>
             <ul class="dropdown-menu">
               <li><router-link class="dropdown-item" :to = "{name: 'user_bot_index'}">My Bot</router-link></li>
               <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Logout</a></li>
+              <li><a class="dropdown-item" href="#" @click="logout">Logout</a></li>
             </ul>
           </li>
         </ul>
+        <ul class="navbar-nav" v-else>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'user_account_login' }" role="button">
+              登录
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'user_account_register'}" role="button">
+              注册
+            </router-link>
+          </li>
+        </ul>
+
       </div>
     </div>
   </nav>
@@ -34,14 +47,21 @@
 <script>
 import { useRoute } from 'vue-router'
 import { computed } from 'vue'
+import { useStore } from 'vuex';
+
 export default {
   setup() {
     const route = useRoute();
+    const store = useStore();
     let route_name = computed(() => {
       return route.name
     });
+    const logout = () => {
+      store.dispatch("logout");
+    };
     return {
-      route_name
+      route_name,
+      logout
     }
   }
 }
